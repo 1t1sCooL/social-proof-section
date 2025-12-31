92e4543aab983865dbfeca68beea9f8af76a98ee
1t1sCooL <mmalabugin@gmail.com>
2025-12-31 20:44:36 +0300

    fix for vercel

--- src/features/feedbacks/ui/Feedbacks.tsx
+++ src/features/feedbacks/ui/Feedbacks.tsx
@@ -5,21 +5,21 @@ export const Feedbacks = () => {
   return (
     <section className={styles.product__testimonial}>
       <UsersFeedback
-        imgSrc={"/SocialProofSection/images/image-colton.jpg"}
+        imgSrc={"/images/image-colton.jpg"}
         name={"Colton Smith"}
         feedback={`“ We needed the same printed design as the one we had ordered a week prior. Not only did
                         they find the original order, but we also received it in time. Excellent! ”`}
       />
       <UsersFeedback
         className="margin-top--24"
-        imgSrc={"/SocialProofSection/images/image-irene.jpg"}
+        imgSrc={"/images/image-irene.jpg"}
         name={"Irene Roberts"}
         feedback={`“ Customer service is always excellent and very quick turn around. Completely delighted
                                 with the simplicity of the purchase and the speed of delivery. ”`}
       />
       <UsersFeedback
         className="margin-top--48"
-        imgSrc={"/SocialProofSection/images/image-anne.jpg"}
+        imgSrc={"/images/image-anne.jpg"}
         name={"Anne Wallace"}
         feedback={`“ Put an order with this company and can only praise them for the very high standard.
                                 Will definitely use them again and recommend them to everyone! ”`}
--- src/shared/ui/Stars/Stars.tsx
+++ src/shared/ui/Stars/Stars.tsx
@@ -3,11 +3,11 @@ import styles from "./stars.module.css";
 export const Stars = () => {
   return (
     <div className={styles.stars}>
-      <img src="/SocialProofSection/images/icon-star.svg" alt="rating star" />
-      <img src="/SocialProofSection/images/icon-star.svg" alt="rating star" />
-      <img src="/SocialProofSection/images/icon-star.svg" alt="rating star" />
-      <img src="/SocialProofSection/images/icon-star.svg" alt="rating star" />
-      <img src="/SocialProofSection/images/icon-star.svg" alt="rating star" />
+      <img src="/images/icon-star.svg" alt="rating star" />
+      <img src="/images/icon-star.svg" alt="rating star" />
+      <img src="/images/icon-star.svg" alt="rating star" />
+      <img src="/images/icon-star.svg" alt="rating star" />
+      <img src="/images/icon-star.svg" alt="rating star" />
     </div>
   );
 };
